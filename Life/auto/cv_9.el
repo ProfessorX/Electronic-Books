0d7cb0257e4ec162c9174237aee39c9a541e2645
Abraham Xiao <q289196573@gmail.com>
2015-05-04 09:06:17 +0800

    Updated resume. Added some practical information into my not-so-long CV.

--- Life/auto/cv_9.el
+++ Life/auto/cv_9.el
@@ -1,27 +1,34 @@
-(TeX-add-style-hook "cv_9"
+(TeX-add-style-hook
+ "cv_9"
  (lambda ()
-    (LaTeX-add-labels
-     "sec:career-object"
-     "sec:course-projects")
-    (TeX-add-symbols
-     "fb")
-    (TeX-run-style-hooks
-     "titlesec"
-     "hyperref"
-     "layaureo"
-     "big"
-     "xcolor"
-     "dvipsnames"
-     "usenames"
-     "parskip"
-     "url"
-     "xltxtra"
-     "xunicode"
-     "fontspec"
-     ""
-     "latex2e"
-     "art10"
-     "article"
-     "10pt"
-     "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "10pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "usenames" "dvipsnames") ("layaureo" "big")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fontspec"
+    "xunicode"
+    "xltxtra"
+    "url"
+    "parskip"
+    "xcolor"
+    "layaureo"
+    "hyperref"
+    "titlesec")
+   (TeX-add-symbols
+    "fb")
+   (LaTeX-add-labels
+    "sec:career-object"
+    "sec:course-projects")))
 
